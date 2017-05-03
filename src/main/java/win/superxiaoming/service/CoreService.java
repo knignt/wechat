bb7abcd709f7a347af8373c815263f3aa7131d0c
wu <1072124335@qq.com>
2017-05-03 17:47:59 +0800

    first add

---
+++ src/main/java/win/superxiaoming/service/CoreService.java
@@ -0,0 +1,15 @@
+package win.superxiaoming.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CoreService {
+
+	public CoreService() {
+		super();
+	}
+	
+	public void doService(HttpServletRequest request,HttpServletResponse response){
+		
+	}
+}
